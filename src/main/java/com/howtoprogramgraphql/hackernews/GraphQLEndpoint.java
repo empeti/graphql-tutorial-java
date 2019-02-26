1d97a318dfc416f54d6922d92b126f432078b108
Peter Matyas <Peter_Matyas@epam.com>
2019-02-26 16:50:21 +0100

    Get all links via GraphQL Query

---
+++ src/main/java/com/howtoprogramgraphql/hackernews/GraphQLEndpoint.java
@@ -0,0 +1,22 @@
+package com.howtoprogramgraphql.hackernews;
+
+import com.coxautodev.graphql.tools.SchemaParser;
+import graphql.schema.GraphQLSchema;
+import graphql.servlet.SimpleGraphQLServlet;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/graphql")
+public class GraphQLEndpoint extends SimpleGraphQLServlet{
+    public GraphQLEndpoint() {
+        super(buildSchema());
+    }
+
+    private static GraphQLSchema buildSchema() {
+        LinkRepository linkRepository = new LinkRepository();
+        return SchemaParser.newParser()
+                .file("schema.graphqls")
+                .resolvers(new Query(linkRepository))
+                .build()
+                .makeExecutableSchema();
+    }
+}
---
+++ src/main/java/com/howtoprogramgraphql/hackernews/LinkRepository.java
@@ -0,0 +1,22 @@
+package com.howtoprogramgraphql.hackernews;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LinkRepository {
+    private final List<Link> links;
+
+    public LinkRepository() {
+        this.links = new ArrayList<>();
+        links.add(new Link("http://howtographql.com", "Your favorite GraphQL page"));
+        links.add(new Link("http://graphql.org/learn/", "The official docks"));
+    }
+
+    public List<Link> getAllLinks(){
+        return links;
+    }
+
+    public void saveLink(Link link){
+        links.add(link);
+    }
+}
